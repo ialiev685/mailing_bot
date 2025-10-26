b3ad1896e9373ffbe666a1d770608173ef8e2173
ilfat aliev <ialiev685@gmail.com>
2025-10-26 17:06:17 +0300

    fix error for test on VPS

--- tests/core_testing.py
+++ tests/core_testing.py
@@ -16,10 +16,14 @@ DB_USER = os.getenv("POSTGRES_USER")
 DB_PASSWORD = os.getenv("POSTGRES_PASSWORD")
 DB_NAME = os.getenv("POSTGRES_DB")
 POSTGRES_HOST_TESTING = os.getenv("POSTGRES_HOST_TESTING")
+DB_PORT = os.getenv("DB_PORT")
 DB_PORT_TESTING = os.getenv("POSTGRES_PORT_TESTING")
 DB_NAME_TESTING = os.getenv("POSTGRES_DB_TESTING")
 
-DATABASE_URL = f"postgresql://{DB_USER}:{DB_PASSWORD}@{POSTGRES_HOST_TESTING}:{DB_PORT_TESTING}/{DB_NAME_TESTING}"
+# Так запуска app в образке docker, обращение идет через внутренний port (внешний:внутренний)
+PORT = DB_PORT_TESTING if POSTGRES_HOST_TESTING == "localhost" else DB_PORT
+
+DATABASE_URL = f"postgresql://{DB_USER}:{DB_PASSWORD}@{POSTGRES_HOST_TESTING}:{PORT}/{DB_NAME_TESTING}"
 
 
 @pytest.fixture
