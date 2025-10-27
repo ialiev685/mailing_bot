f5910ded9b1160dc517b1fe95ce108f5525b21ab
ilfat aliev <ialiev685@gmail.com>
2025-10-27 21:56:17 +0300

    update workflow

--- database/core.py
+++ database/core.py
@@ -10,7 +10,7 @@ DB_USER = os.getenv("POSTGRES_USER")
 DB_PASSWORD = os.getenv("POSTGRES_PASSWORD")
 DB_NAME = os.getenv("POSTGRES_DB")
 DB_HOST = os.getenv("POSTGRES_HOST")
-DB_PORT = os.getenv("POSTGRES_PORT")
+DB_PORT = os.getenv("POSTGRES_PORT", 5432)
 
 
 DATABASE_URL = f"postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
