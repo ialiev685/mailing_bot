e3b5f37eb0d6518d677981f5904c95dd478ff20f
ilfat aliev <ialiev685@gmail.com>
2025-11-15 16:26:19 +0300

    добавил название хоста по умолчанию для базы данных

--- alembic/env.py
+++ alembic/env.py
@@ -12,7 +12,7 @@ init_env_file()
 DB_USER = os.getenv("POSTGRES_USER")
 DB_PASSWORD = os.getenv("POSTGRES_PASSWORD")
 DB_NAME = os.getenv("POSTGRES_DB")
-DB_HOST = os.getenv("POSTGRES_HOST")
+DB_HOST = os.getenv("POSTGRES_HOST", "localhost")
 DB_PORT = os.getenv("POSTGRES_PORT")
 
 SQLALCHEMY_URL = f"postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
--- database/core.py
+++ database/core.py
@@ -7,7 +7,7 @@ init_env_file()
 DB_USER = os.getenv("POSTGRES_USER")
 DB_PASSWORD = os.getenv("POSTGRES_PASSWORD")
 DB_NAME = os.getenv("POSTGRES_DB")
-DB_HOST = os.getenv("POSTGRES_HOST")
+DB_HOST = os.getenv("POSTGRES_HOST", "localhost")
 DB_PORT = os.getenv("POSTGRES_PORT", 5432)
 
 
